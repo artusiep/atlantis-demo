5b967e102d5533a7f1fa36c23876bf7fc7cc883b
Artur Siepietowski <artursiepietowski@gmail.com>
2022-08-01 17:40:32 +0200

    Example bucket

---
+++ terraform/artusiep-worker-1/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_version = "~> 1.1"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.4"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.12"
+    }
+  }
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "artusiep-terraform-state"
+    prefix = "artusiep-worker-1"
+  }
+}
+
+locals {
+  region          = "europe-west3"
+  project         = "artusiep-worker-1"
+  billing_account = "015ED4-E4FEC0-B83F49"
+}
+
+provider "google" {
+  region  = local.region
+  project = local.project
+}
+
+resource "google_project" "artusiep_worker_1" {
+  name            = local.project
+  project_id      = local.project
+  billing_account = local.billing_account
+}
+
+
+resource "google_storage_bucket" "state_bucket" {
+  name     = "artusiep-terraform-state"
+  location = "EU"
+  project  = google_project.artusiep_worker_1.project_id
+}
